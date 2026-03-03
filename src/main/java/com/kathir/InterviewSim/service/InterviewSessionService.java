586094de6de72ad6e8ea234a0a55223bcba26a12
Kathir G <kathirganesan11@gmail.com>
2026-03-03 23:28:21 +0530

    Add InterviewSessionService interface

---
+++ src/main/java/com/kathir/InterviewSim/service/InterviewSessionService.java
@@ -0,0 +1,7 @@
+package com.kathir.InterviewSim.service;
+import com.kathir.InterviewSim.entity.*;
+public interface InterviewSessionService {
+    InterviewSession startSession(String problemStatement);
+    InterviewSession getSession(Long id);
+    InterviewSession submitAnswer(Long sessionId,String Answer);
+}
---
+++ src/main/java/com/kathir/InterviewSim/service/InterviewSessionServiceImpl.java
@@ -0,0 +1,4 @@
+package com.kathir.InterviewSim.service;
+
+public class InterviewSessionServiceImpl {
+}
