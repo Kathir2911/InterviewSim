74a7ec68a6977669c87e90afcb65faa5c1b98d63
Kathir G <kathirganesan11@gmail.com>
2026-03-06 22:51:11 +0530

    Controller created and updated

---
+++ src/main/java/com/kathir/InterviewSim/controller/InterviewController.java
@@ -0,0 +1,27 @@
+package com.kathir.InterviewSim.controller;
+import com.kathir.InterviewSim.service.InterviewSessionService;
+import com.kathir.InterviewSim.dto.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/interview")
+public class InterviewController {
+    private final InterviewSessionService service;
+
+    @PostMapping("/start")
+    public InterviewSessionResponse startInterview(@RequestBody StartInterviewRequest request){
+        return service.startSession(request.getProblemStatement());
+    }
+
+    @PostMapping("/{id}/answer")
+    public InterviewSessionResponse submitAnswer(@PathVariable Long id,@RequestBody SubmitAnswerRequest request){
+        return service.submitAnswer(id,request.getAnswer());
+    }
+
+    @GetMapping("/{id}")
+    public InterviewSessionResponse getSession(@PathVariable Long id) {
+        return service.getSession(id);
+    }
+}
